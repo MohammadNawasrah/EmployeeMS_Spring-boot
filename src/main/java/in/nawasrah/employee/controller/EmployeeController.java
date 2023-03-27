8ff495d84823d8290992315a36e15dfc55be22dd
Mohammad Al Nawasrah <nawasrahmohammad2000@gmail.com>
2023-03-27 17:43:46 +0300

    turn on displayAll employee

--- src/main/java/in/nawasrah/employee/controller/EmployeeController.java
+++ src/main/java/in/nawasrah/employee/controller/EmployeeController.java
@@ -3,21 +3,25 @@ package in.nawasrah.employee.controller;
 //import in.nawasrah.employee.database.DB;
 //import in.nawasrah.employee.database.DbSql;
 
+import in.nawasrah.employee.model.Employee;
 import in.nawasrah.employee.repository.EmployeeRepos;
+import in.nawasrah.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping(value = "/employees")
 
 @RestController
 public class EmployeeController {
     @Autowired
-    EmployeeRepos employeeRepos;
+    EmployeeService employeeService;
 
     @GetMapping("/")
-    public String displayEmployees() {
-        return "displayEmployees" + employeeRepos.findAll();
+    public List<Employee> displayEmployees() {
+        return  employeeService.getAllEmployee();
     }
 }

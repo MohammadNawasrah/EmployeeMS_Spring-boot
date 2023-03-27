8e010e671c2a136ece75c4b5b1f07f0bcfee3e5f
Mohammad Al Nawasrah <nawasrahmohammad2000@gmail.com>
2023-03-27 17:17:05 +0300

    fix bug in dbSql class

--- src/main/java/in/nawasrah/employee/database/DbSql.java
+++ src/main/java/in/nawasrah/employee/database/DbSql.java
@@ -6,14 +6,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
 @Component
-public class DbSql implements DB{
+public class DbSql implements DB {
     @Override
     public Connection connection(String connectionString) {
         try {
-            Connection con= DriverManager.getConnection(connectionString);
+            Connection con = DriverManager.getConnection(connectionString);
             return con;
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
         }
@@ -22,9 +23,10 @@ public class DbSql implements DB{
     @Override
     public boolean createTable(String sql, Connection con) {
         try {
-            Statement statement=con.createStatement();
-            return statement.execute(sql);
-        }catch (Exception e){
+            Statement statement = con.createStatement();
+            statement.execute(sql);
+            return true;
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return false;
         }
@@ -33,10 +35,10 @@ public class DbSql implements DB{
     @Override
     public ResultSet select(String sql, Connection con) {
         try {
-            Statement statement=con.createStatement();
-            ResultSet resultSet=statement.executeQuery(sql);
+            Statement statement = con.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
             return resultSet;
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
         }
@@ -45,9 +47,10 @@ public class DbSql implements DB{
     @Override
     public boolean insert(String sql, Connection con) {
         try {
-            Statement statement=con.createStatement();
-            return statement.execute(sql);
-        }catch (Exception e){
+            Statement statement = con.createStatement();
+            statement.execute(sql);
+            return true;
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return false;
         }
@@ -56,9 +59,10 @@ public class DbSql implements DB{
     @Override
     public boolean delete(String sql, Connection con) {
         try {
-            Statement statement=con.createStatement();
-            return statement.execute(sql);
-        }catch (Exception e){
+            Statement statement = con.createStatement();
+            statement.execute(sql);
+            return true;
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return false;
         }
@@ -67,9 +71,10 @@ public class DbSql implements DB{
     @Override
     public boolean update(String sql, Connection con) {
         try {
-            Statement statement=con.createStatement();
-            return statement.execute(sql);
-        }catch (Exception e){
+            Statement statement = con.createStatement();
+            statement.execute(sql);
+            return true;
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return false;
         }

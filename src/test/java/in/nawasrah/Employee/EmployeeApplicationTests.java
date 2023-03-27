bae22ca1f6fa7f0e04f4655561521c040f24e5bb
Mohammad Al Nawasrah <nawasrahmohammad2000@gmail.com>
2023-03-27 14:27:55 +0300

    initializr spring boot P

---
+++ src/test/java/in/nawasrah/Employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nawasrah.Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
